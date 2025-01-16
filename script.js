e29f03c437b314dbf046eb7732c2c52703a131ef
Kauan Constancio <kauanconstancio13@gmail.com>
2025-01-16 00:00:19 -0300

    mudando o 'sobre mim' de lugar

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// function filterCertificates(category) {
-//     const items = document.querySelectorAll('.item');
-//     items.forEach(item => {
-//         if (category === 'all' || item.classList.contains(category)) {
-//             item.style.display = 'block';
-//         } else {
-//             item.style.display = 'none';
-//         }
-//     });
-// }
-
-// // Show all items on initial load
-// filterCertificates('all');
 
 function filterCertificates(category) {
     const items = document.querySelectorAll('.item');
