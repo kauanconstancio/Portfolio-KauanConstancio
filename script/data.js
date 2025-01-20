04ffdb7896651b846a0133b4626886ac4d76fb87
Kauan Constancio <kauanconstancio13@gmail.com>
2025-01-20 17:15:35 -0300

    Criando uma data base dos certificados

---
+++ script/data.js
@@ -0,0 +1,7 @@
+let certificatesData = [
+    {
+        image: "..assets/certificados/Captura de tela 2025-01-12 203428.png",
+        link: "..assets/certificados/Captura de tela 2025-01-12 203428.png",
+        class: ".rocketseat .item"
+    }
+];
