55d692b37d08d27d9d8c20b729a8b550d0bf6328
Kauan Constancio <kauanconstancio13@gmail.com>
2025-01-24 01:57:34 -0300

    adicionado banco de dados de certificados

--- script/data.js
+++ script/data.js
@@ -1,7 +1,72 @@
-let certificatesData = [
+let dados = [
     {
-        image: "..assets/certificados/Captura de tela 2025-01-12 203428.png",
-        link: "..assets/certificados/Captura de tela 2025-01-12 203428.png",
+        image: "assets/certificados/Captura de tela 2025-01-12 203204.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203204.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203221.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203221.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203233.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203233.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203244.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203244.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203254.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203254.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203313.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203313.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203325.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203325.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203339.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203339.png",
+        class: ".formations .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203355.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203355.png",
+        class: ".rocketseat .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203442.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203442.png",
+        class: ".bradesco .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203457.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203457.png",
+        class: ".bradesco .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-13 125908.png",
+        link: "assets/certificados/Captura de tela 2025-01-13 125908.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-18 135509.png",
+        link: "assets/certificados/Captura de tela 2025-01-18 135509.png",
+        class: ".alura .item"
+    },
+    {
+        image: "assets/certificados/Captura de tela 2025-01-12 203204.png",
+        link: "assets/certificados/Captura de tela 2025-01-12 203428.png",
         class: ".rocketseat .item"
     }
 ];
--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
+let listaCertificados = document.getElementById('listaCertificados');
+
+for (let dado of dados) {
+
+listaCertificados.innerHTML += `
+<li class="${dado.class}">
+    <img src="${dado.image}">
+    <a href="${dado.link}"target="_blank">Ver Certificado</a>
+</li>`;
+}
+
 
 function filterCertificates(category) {
     const items = document.querySelectorAll('.item');
@@ -12,3 +23,4 @@ function filterCertificates(category) {
 
 // Show all items on initial load
 filterCertificates('all');
+
