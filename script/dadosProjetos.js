9bc62178d97459c6dd3ccf1b11086e6db50ca5bb
Kauan Constancio <kauanconstancio13@gmail.com>
2025-01-24 20:28:39 -0300

    criando banco de dados dos projetos

---
+++ script/dadosProjetos.js
@@ -0,0 +1,20 @@
+let dadosProjetos = [
+    {
+        image: "assets/projects-img/Captura de tela 2025-01-24 201255.png",
+        title: "Calculadora de medias",
+        linkProjeto: "https://calculador-de-medias-zeta.vercel.app/",
+        linkGithub: "https://github.com/kauanconstancio/calculador-de-medias"
+    },
+    {
+        image: "assets/projects-img/Captura de tela 2025-01-13 124340.png",
+        title: "Adivinhação de números",
+        linkProjeto: "https://l-gica-com-js.vercel.app/",
+        linkGithub: "https://github.com/kauanconstancio/L-gica-com-JS/tree/main/parte-2/numero%20secreto%202.0"
+    },
+    {
+        image: "assets/projects-img/Captura de tela 2025-01-12 174914.png",
+        title: "Biblioteca de jogos",
+        linkProjeto: "https://imerss-o-dev-alura-google-gemini.vercel.app/",
+        linkGithub: "https://github.com/kauanconstancio/Imersao-DEV-Alura-Google-Gemini"
+    },
+]
