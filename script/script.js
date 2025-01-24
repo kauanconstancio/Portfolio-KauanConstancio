3849e86f922b14229a548cc066a8fb96ed32bac6
Kauan Constancio <kauanconstancio13@gmail.com>
2025-01-24 20:29:01 -0300

    criando innerHTML para a lista de projetos

--- script/script.js
+++ script/script.js
@@ -1,12 +1,28 @@
+let listaProjetos = document.getElementById('listaProjetos');
+
+for (let projeto of dadosProjetos) {
+    listaProjetos.innerHTML += `
+    <div class="project">
+        <img src="${projeto.image}">
+        <div class="description">
+            <h2>${projeto.title}</h2>
+            <div class="links">
+                <a href="${projeto.linkProjeto}" target="_blank">Ver Projeto</a>
+                <a href="${projeto.linkGithub}" target="_blank">GitHub do Projeto</a>
+            </div>
+        </div>
+    </div>
+    `
+}
+
 let listaCertificados = document.getElementById('listaCertificados');
 
 for (let dado of dados) {
-
-listaCertificados.innerHTML += `
-<li class="${dado.class}">
-    <img src="${dado.image}">
-    <a href="${dado.link}"target="_blank">Ver Certificado</a>
-</li>`;
+    listaCertificados.innerHTML += `
+    <li class="${dado.class}">
+        <img src="${dado.image}">
+        <a href="${dado.link}"target="_blank">Ver Certificado</a>
+    </li>`;
 }
 
 
