bcc3345869ded235c46b4e623d2f220fd92befe0
Kauan Constancio <kauanconstancio13@gmail.com>
2025-07-17 02:36:00 -0300

    fazendo página de certificados e JS de mostrar imagem do certificado

---
+++ portfolioManager/script/certificados.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+      const inputFile = document.querySelector(".imageInput");
+      const pictureImage = document.querySelector(".picture__image");
+      const pictureImageTxt = '<i class="fa-solid fa-plus"></i> Adicionar imagem do projeto';
+      pictureImage.innerHTML = pictureImageTxt;
+
+      inputFile.addEventListener("change", function (e) {
+        const file = e.target.files[0];
+        if (file && file.type.startsWith("image/")) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            const img = document.createElement("img");
+            img.src = e.target.result;
+            img.classList.add("picture__img");
+            pictureImage.innerHTML = "";
+            pictureImage.appendChild(img);
+          };
+          reader.readAsDataURL(file);
+        } else {
+          pictureImage.innerHTML = pictureImageTxt;
+        }
+      });
+    });
